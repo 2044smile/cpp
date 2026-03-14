8bbe860e78fd7c1680f77cefa87e38ea93b8a77b
2044smile <2044smile@naver.com>
2026-03-14 12:33:49 +0900

    ✨ Feat: 1.10 선언과 정의의 분리

---
+++ basic/따라하며 배우는 C++/1.10 선언과 정의의 분리/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b); // forward declaration
+int multiply(int a, int b);
+int subtract(int a, int b);
+
+int main()
+{
+    cout << add(1, 2) << endl;
+    cout << multiply(3, 4) << endl;
+    cout << subtract(5, 6) << endl;
+
+    return 0;
+}
+
+
+int add(int a, int b){
+    return a + b;
+}
+int multiply(int a, int b)
+{
+    return a * b;
+}
+int subtract(int a, int b)
+{
+    return a - b;
+}
