7dbf90230c4f7b9397300c1b0c620f2f5d378bb5
2044smile <2044smile@naver.com>
2026-03-14 12:06:30 +0900

    ✨ Feat: 1.9 기본적인 서식 맞추기

---
+++ basic/따라하며 배우는 C++/1.9 기본적인 서식 맞추기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int x, int y) { return x + y; }
+
+int mul(int x, int y) { return x * y; }
+
+int main()
+{
+    int my_v           = 1;
+    int x              = 4;
+    int num_appels     = 123;
+
+    cout << "Hello, World!" << "abcdef" << 
+    "Hello Home" << endl;
+
+    return 0;
+}
