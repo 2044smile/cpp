f5619da393434fcd405c7b757750f19bc985d5cd
2044smile <2044smile@naver.com>
2026-03-30 16:19:11 +0900

    💩 Feat: I am solving some problem

---
+++ basic/따라하며 배우는 C++/3. 변수와 기본적인 자료형/2.5 부동소수점 수 floating point numbers/Question/1.cpp
@@ -0,0 +1,20 @@
+// 3.141592
+// 31.41
+// 0.001000
+// 123.0002
+// 0.0000000001
+// 234560000.0000
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double t1 = 3.141592;
+    double t2 = 3.141e1;
+    double t3 = 1e-3;
+
+    cout << t1 << endl;
+    cout << t2 << endl;
+    cout << t3 << endl;
+}
