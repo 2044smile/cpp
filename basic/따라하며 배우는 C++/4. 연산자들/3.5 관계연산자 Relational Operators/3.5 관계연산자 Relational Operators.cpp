9c7dcbfe931f2b52c263e5da8317830f8a1d2748
2044smile <2044smile@naver.com>
2026-04-13 07:25:41 +0900

    ✨ Feat: 3.5 관계연산자 Relational Operators 숫자가 클수록 허용 오차도 같이 커진다. 혼합 방식으로 함수화해두는 것도 좋아보인다.

--- basic/따라하며 배우는 C++/4. 연산자들/3.5 관계연산자 Relational Operators/3.5 관계연산자 Relational Operators.cpp
+++ basic/따라하며 배우는 C++/4. 연산자들/3.5 관계연산자 Relational Operators/3.5 관계연산자 Relational Operators.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 int main()
@@ -23,6 +24,35 @@ int main()
             cout << "x is greater than y or equal to y" << endl;
         if (x <= y)
             cout << "x is less than or equal to y" << endl;
+    }
+    double d1(100 - 99.99);     // 0.01
+    double d2(10 - 9.99);       // 0.01
+
+    cout << d1 << ", " << d2 << endl;
+    cout << std::abs(d1 - d2) << endl;  // 5.32907e-15
+
+    const double epsilon = 1e-10;
+    if(std::abs(d1 - d2) < epsilon)
+        cout << "Approximately equal" << endl;
+    else   
+        cout << "Not equal" << endl;
+
+    cout << "--------------------" << endl;
+    cout << sizeof(d1) << endl;     // 8
+    cout << sizeof(d2) << endl;     // 8
+    cout << std::setprecision(17);  // using iomanip
+    cout << d1 << endl;             // 0.010000000000005116
+    cout << d2 << endl;             // 0.0099999999999997868
+
+    if (d1 == d2)
+        cout << "equal" << endl;
+    else
+    {
+        cout << "not equal" << endl;
+
+        if (d1 > d2) cout << "d1 > d2" << endl;
+        else    // if(d1 < d2) because d1 != d2
+            cout << "d1 < d2" << endl;
     }
 
     return 0;
