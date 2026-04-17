b2b7ee8ce833b71ce203ec84f0952250024314c7
2044smile <2044smile@naver.com>
2026-04-17 10:51:11 +0900

    🩹 Fix: minor

--- algorithms/backjoon/1094.cpp
+++ algorithms/backjoon/1094.cpp
@@ -41,4 +41,29 @@ int main()
 
     return 0;   
 }
+// 개선
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int X;
+    int cnt = 0;
+
+    cin >> X;
+
+    while(X != 0)
+    {
+        if (X % 2 == 1)
+            cnt++;
+        X /= 2;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
