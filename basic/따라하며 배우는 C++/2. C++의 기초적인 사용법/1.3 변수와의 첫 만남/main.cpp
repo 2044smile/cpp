524424fb857ab149d3b7e339ba5e7e1b43b94741
2044smile <2044smile@naver.com>
2026-04-25 15:11:08 +0900

    ♻️ Feat: 1.3 변수와의 첫 만남
    [복습] range-based for, std::move(), something.size()

--- basic/따라하며 배우는 C++/2. C++의 기초적인 사용법/1.3 변수와의 첫 만남/main.cpp
+++ basic/따라하며 배우는 C++/2. C++의 기초적인 사용법/1.3 변수와의 첫 만남/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
     // int x;
     // int x = 10; initialization 
@@ -20,6 +22,24 @@ int main() {
 
     int z;
     std::cout << z << std::endl; // 초기화 되지 않은 변수는 쓰레기 값(-155200824)이 출력됨
+
+    cout << "-------------" << endl;
+    vector<int> v1 = {1,2,3};
+    vector<int> v2 = std::move(v1); //! v1의 자원을 v2에 넘겨라, 대용량 객체 복사 비용을 0에 가깝게 줄인다, 
+
+    cout << "v1 size: " << v1.size() << endl;   //& 0
+    cout << "v2 size: " << v2.size() << endl;   //& 3
+
+    //^ 몇 번째 인지 필요하면
+    // for (int i = 0; i < v.size(); i++)
+
+    //^ range-based for 값만 필요하면
+    for(int i : v1){
+        cout << i << endl;
+    }
+    for(int j : v2){
+        cout << j << endl;          // 1, 2, 3
+    }
 
     return 0;
 }
