d3fc7fff0862c841986953bc9f32659be34a28f3
2044smile <2044smile@naver.com>
2026-04-27 09:16:39 +0900

    💡 Feat: added another answer and I found some minor issue

--- algorithms/programmers/코딩 기초 트레이닝/주사위 게임.cpp
+++ algorithms/programmers/코딩 기초 트레이닝/주사위 게임.cpp
@@ -31,3 +31,40 @@ int main() {
 		cout << a << " " << b << " " << result << endl;
 	}
 }
+//! ---
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if(a%2 == 1 & b%2 == 1){	//! &(비트 AND): 비트끼리 연산, &&(논리 AND): 조건이 둘 다 참인지 확인
+        a = a*a;				// 정답으로 인정된 건 % 연산 결과가 0 또는 1이여서, 숫자끼리 비교할 땐 결과가 다르다
+        b = b*b;
+        answer = a+b;
+    }
+    if (a%2 == 0 & a%2 ==0) {
+        answer = abs(a-b);
+    }
+    if((a%2 == 1 & b%2 == 0) | (a%2 == 0 & b%2 == 1)){
+        a *= 2;
+        b *= 2;
+        answer = a+b;
+    }
+    return answer;
+}
+//* another answer
+#include <cstdlib>	// abs() 함수를 쓰기 위해 포함한 헤더
+
+using namespace std;
+
+int  solution(int a, int b) {
+	int answer = 0;
+
+	if(a%2 == 1 && b%2==1) answer = (a*a) + (b*b);
+	else if((a%2 == 1 && b%2 == 0) || (a%2 == 0 && b%2 == 1)) answer = 2 * (a+b);
+	else answer = abs(a-b);
+	return answer;
+}
