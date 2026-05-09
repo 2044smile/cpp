c393494c249843185dfb93ef7e22c537e112d0b3
2044smile <2044smile@naver.com>
2026-05-09 22:47:07 +0900

    📝 Docs: added some description
    it's super difficult

--- basic/따라하며 배우는 C++/5. 변수 범위와 더 다양한 변수형/4.2 전역 변수, 정적 변수, 내부 연결, 외부 연결/4.2 전역 변수, 정적 변수, 내부 연결, 외부 연결.cpp
+++ basic/따라하며 배우는 C++/5. 변수 범위와 더 다양한 변수형/4.2 전역 변수, 정적 변수, 내부 연결, 외부 연결/4.2 전역 변수, 정적 변수, 내부 연결, 외부 연결.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include "constants.h"
 
 using namespace std;
 
 int value = 123;                // global variable
 int g_a = 1;                    // global variable
+static int s_a = 1;             // static(정적) variable 
+
+// forward declaration
+// main.cpp 를 컴파일 할 때 externalDoSomething() 함수의 body 가 존재하니까 linking 할 때 가져다 붙이세요
+extern void externalDoSomething();  //! extern 키워드 생략 가능
+extern int vVv;                     //! extern 키워드 생략 가능
 
 void doSomething()
 {
     // int a = 1;
-    // static int a = 1;        // static variable 디버깅, 테스팅 할 때 사용, static
+    // static int a = 1;
     //! static은 변수 a가 OS로 부터 받은 메모리가 static, 초기화를 한번 밖에 안한다
     //! static 변수를 선언할 때 초기화가 반드시 있어야 한다. [X] static int a [O] static int a = 1
     ++g_a;
     cout << g_a << endl;
+}
+
+void foo() {
+    int local = 0;              // local variable 함수가 호출될 때 마다 생성되고, 함수 호출 - 함수 종료하면 소멸, stack 메모리에 저장, 함수 밖에서 접근 불가
+    static int stat = 0;        // static variable 함수가 호출될 때 단 한 번만 생성되고, 프로그램이 끝날 때까지 유지, data segement에 저장, 함수 밖에서 접근 불가
+
+    local++;
+    stat++;
+    cout << "local: " << local << ", static: " << stat << endl;
 }
 
 int main()
@@ -27,6 +43,20 @@ int main()
     doSomething();
     doSomething();
     doSomething();
+
+    cout << "---------" << endl;
+    foo();  // local: 1, static: 1
+    foo();  // local: 1, static: 2
+    foo();  // local: 1, static: 3
+
+    cout << "---------" << endl;
+    externalDoSomething();
+    cout << vVv << endl;
+
+    cout << "---------" << endl;
+    cout << Constants::pi << endl;
+    cout << Constants::gravity << endl;
+    cout << "[main.cpp] &Constants::pi = " << Constants::pi << " " << &Constants::pi << endl;
 
     return 0;
 }
