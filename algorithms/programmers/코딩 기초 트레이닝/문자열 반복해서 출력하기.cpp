925de59616987cac9f4d76251764c6e2de7ca8ba
2044smile <2044smile@naver.com>
2026-05-11 08:25:09 +0900

    🩹 Fix: added a description

--- algorithms/programmers/코딩 기초 트레이닝/문자열 반복해서 출력하기.cpp
+++ algorithms/programmers/코딩 기초 트레이닝/문자열 반복해서 출력하기.cpp
@@ -7,6 +7,8 @@ int main(void) {
     string str;
     int n;
     cin >> str >> n;
+    // int i; 로 선언하면 i 에는 그 시점 스택 메모리에 남아있던 쓰레기 값이 들어간다
+    //! C++은 Python과 달리 변수를 선언해도 자동으로 0으로 초기화해주지 않습니다. 반드시 직접 초기화해야 합니다
     for(int i=0; i<n; i++){
         cout << str;
     }
