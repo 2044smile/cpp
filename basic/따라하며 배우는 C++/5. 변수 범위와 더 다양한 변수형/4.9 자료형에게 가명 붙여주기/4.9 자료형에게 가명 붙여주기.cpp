1b334ff6838f2a95ab3583bfcd65dc3ef46fbe7c
2044smile <2044smile@naver.com>
2026-05-25 21:48:15 +0900

    ✨ Feat: 4.9 자료형에게 가명 붙여주기

---
+++ basic/따라하며 배우는 C++/5. 변수 범위와 더 다양한 변수형/4.9 자료형에게 가명 붙여주기/4.9 자료형에게 가명 붙여주기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+
+int main()
+{
+    using namespace std;
+
+    typedef double distance_t;      // _t = type
+    // typedef float distance_t;    // 변수의 자료형을 손쉽게 변경 가능
+
+    // std::int8_t i(97);          // _t 가명
+
+    double      my_distance;
+    distance_t  home2work;
+    distance_t  home2school;
+    cout << "------------" << endl;
+
+
+    return 0;
+}
